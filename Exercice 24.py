c8d4594ba5901f0ea6e4ec9d9a3834a1de00fdf5
gnorgol <52493523+gnorgol@users.noreply.github.com>
2020-11-09 14:33:21 +0100

    Modif exercice 24

--- Exercice 24.py
+++ Exercice 24.py
@@ -1,16 +1,10 @@
-s = input("Entrez une phrase : ")
-i=0
-Tab = []
-Tab=["a","z","e","r","t","y","u","i","o","p","q","s","d","f","g","h","j","k","l","m","w","x","c","v","b","n"]
-Resultat=[0]*26
-Solution=[1]*26
-for i in range (0,len(N),1):
-    for j in range (0,len(Tab),1):
-        if (s[i]==Tab[j]):
-            Resultat[j]=1
-
-if (Solution == Resultat) :
+s = input("Entrez un mot : ")
+i = len(s)
+resultat = ""
+while (i != 0):
+    resultat = resultat + s[i-1]
+    i=i-1
+if (resultat == s) :
     print ("C'est un palindrome ")
 else :
     print ("C'est pas un palindrome ")
-print(Resultat)
